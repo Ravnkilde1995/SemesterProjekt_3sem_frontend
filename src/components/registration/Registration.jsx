b4346aa5048b8174df33f862e46cffaee8b37547
ravnkilde1995 <ravnkilde1995@gmail.com>
2023-05-03 13:30:43 +0200

    frontend can now hold user details from signup form

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, {useState,setState} from 'react';
 import "./registration.css";
 
 const Registration = () => {
+
+    const [username, setUsername] = useState(null);
+    const [password,setPassword] = useState(null);
+    const [confirmPassword,setConfirmPassword] = useState(null);
     
-    
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "username"){
+            setUsername(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+        if(id === "confirmPassword"){
+            setConfirmPassword(value);
+        }
+
+    }
+
+    const handleSubmit  = () => {
+        console.log(username,password,confirmPassword);
+    }
+
     return(
-<>
         
 
       <div className="form">
-          <div className="form-body">
-              <div className="username">
-                  <label className="form__label" for="username">Username </label>
-                  <input className="form__input" type="text" id="username" placeholder="Username"/>
-              </div>
-              <div className="password">
-                  <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
-              <div class="footer">
-              <button type="submit" class="btn">Register</button>
-              </div>
-         </div>
-      </div>
-
-      </>
+            <div className="form-body">
+                <div className="username">
+                    <label className="form__label" for="username">Username </label>
+                    <input className="form__input" type="text" value={username} onChange = {(e) => handleInputChange(e)} id="username" placeholder="Username"/>
+                </div>
+                <div className="password">
+                    <label className="form__label" for="password">Password </label>
+                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
+                </div>
+                <div className="confirm-password">
+                    <label className="form__label" for="confirmPassword">Confirm Password </label>
+                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+                </div>
+            </div>
+            <div class="footer">
+                <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
+            </div>
+        </div>
           )
           ;
 }

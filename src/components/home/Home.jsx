72e45a19e059ee622f95b41b1ba26e7e1cf7ae8c
Ravnkilde1995 <ravnkilde1995@gmail.com>
2023-05-16 11:08:54 +0200

    added styling to home and about

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,18 +4,27 @@ import "./home.css";
 const Home = () => {
     return (
         <div>
-            <div className="jumbotron">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">
-                    This is a simple hero unit, a simple jumbotron-style component for calling extra
-                    attention to featured content or information.
+            <div className="jumbotron bg-primary text-left">
+                <h1 className="display-4 text-white">Hello!</h1>
+                <p className="lead text-white">
+                    Welcome to our Single Page Application
                 </p>
-                <hr className="my-4" />
-                <p>
-                    It uses utility classes for typography and spacing to space content out within the larger
-                    container.
+                <hr className="my-4 text-white" />
+                <div className="text-dark">
+                <blockquote class="blockquote ">
+                <p class="mb-0">“Wisdom…. comes not from age, but from education and learning.”</p>
+                <br />
+                <footer class="blockquote-footer text-warning">Anton Chekhov <cite title="Source Title"></cite></footer>
+                </blockquote>
+                </div>
+
+                <p className="text-white">
+                This single page application (SPA) has a google api attached which contains ebooks. 
+                This project is an exam project in SYS made by Abdullah, Nikolaj, Christoffer and Oliver.
+                The project is set up with JPA & we retrieve/create data from our endpoints in the deployed backend. 
+                Enjoy.
                 </p>
-                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <a className="btn btn-warning btn-lg" href="#" role="button">Learn more</a>
             </div>
         </div>
     );
